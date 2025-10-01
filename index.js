bc9a9e4a7e24e10649b702f719010a0601e0e389
Maxime <maxime.lourdelus@gmail.com>
2025-10-01 16:58:34 +0200

    en cours de développement

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const { occurence } = require('./occurence.js');
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
 
 app.use(express.json());
 
 variable = occurence("J'aime les frites et les hamburgers", "e");
-console.log(variable);
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+router.get('./', async (req, res) => {
+  const get = await get.create({
+    chaine1: req.body.chaine1,
+    caractere: req.body.caractere,
+  })
+})
 
 app.listen(3000);
